2e9348c3bdb1a6a0144713b661e75ee54a9ab370
ConradOsvik <conrad.tinius.osvik@gmail.com>
2020-06-21 23:32:24 +0200

    added like feature
    added like feature

--- main.js
+++ main.js
@@ -3,24 +3,89 @@ const crosshairs = db.collection('crosshairs');
 
 const displayDB = document.querySelector('.displayDB');
 
-const getCrosshairs = async () => {
-    displayDB.innerHTML = '';
-    const ans = await crosshairs.get();
-    for(const crosshair of ans.docs){
-        createHTML(crosshair.id, crosshair.data());
+crosshairs.onSnapshot(snap => {
+    for(const ans of snap.docChanges()){
+        if(ans.type === "added"){
+            displayDB.innerHTML += `
+                <div class="crosshair-container" id="${ans.doc.id}">
+                    <div class="more-box">
+                        <span onclick="showMore(this)" class="material-icons dropdownBtn">more_vert</span>
+                        <div id="dropdown" class="more-box-dropdown">
+                            <a>
+                                <span class="material-icons">content_copy</span><span>Copy</span>                    
+                            </a>
+                            <a>
+                                <span class="material-icons" onclick="likeCrosshair(${ans.doc.id})">whatshot</span><span>Like</span>                    
+                            </a>
+                        </div>
+                    </div>
+                    <img src="${ans.doc.data().img}" alt="missing img">
+                    <h3>Description</h3>
+                    <p>${ans.doc.data().desc}</p>
+                    <h3>Crosshair Code:</h3>
+                    <p>${ans.doc.data().crosshair}</p>
+                    <h3>Likes:</h3>
+                    <div class="likes-box">
+                        <span class="material-icons">whatshot</span>
+                        <p>${ans.doc.data().likeCount}</p>            
+                    </div>
+                </div>
+            `;
+        }
+        else if(ans.type === "modified"){
+            let docItem = document.getElementById(ans.doc.id)
+            if(docItem){
+                docItem.innerHTML = `
+                    <div class="more-box">
+                        <span onclick="showMore(this)" class="material-icons dropdownBtn">more_vert</span>
+                        <div id="dropdown" class="more-box-dropdown">
+                            <a>
+                                <span class="material-icons">content_copy</span><span>Copy</span>                    
+                            </a>
+                            <a>
+                                <span class="material-icons" onclick="likeCrosshair(${ans.doc.id})">whatshot</span><span>Like</span>                    
+                            </a>
+                        </div>
+                    </div>
+                    <img src="${ans.doc.data().img}" alt="missing img">
+                    <h3>Description</h3>
+                    <p>${ans.doc.data().desc}</p>
+                    <h3>Crosshair Code:</h3>
+                    <p>${ans.doc.data().crosshair}</p>
+                    <h3>Likes:</h3>
+                    <div class="likes-box">
+                        <span class="material-icons">whatshot</span>
+                        <p>${ans.doc.data().likeCount}</p>            
+                    </div>
+                `
+            }
+        }
+    }
+});
+
+const showMore = function(target) {
+    target.parentNode.querySelector('.more-box-dropdown').classList.toggle("show");
+}
+  
+window.onclick = function(event) {
+    if (!event.target.matches('.dropdownBtn')) {
+        var dropdowns = document.getElementsByClassName("more-box-dropdown");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+        }
+        }
     }
 }
 
-getCrosshairs();
-
-const createHTML = (id, data) => {
-    displayDB.innerHTML += `
-        <div class="crosshair-container">
-            <img src="${data.img}" alt="missing img">
-            <h3>Description</h3>
-            <p>${data.desc}</p>
-            <h3>Crosshair Code:</h3>
-            <p>${data.crosshair}</p>
-        </div>
-    `;
+const likeCrosshair = async (id) => {
+    const doc = await crosshairs.doc(id.toString()).get()
+    const likeCount = doc.data().likeCount;
+    const newLikeCount = likeCount + 1;
+    console.log(newLikeCount);
+    crosshairs.doc(id.toString()).update({
+        likeCount: newLikeCount
+    })
 }
